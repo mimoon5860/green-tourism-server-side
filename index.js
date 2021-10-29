86fa90b1b7d211a98650937bd9a0ef22452045cc
mimoon5860 <mahmodulislammoon@gmail.com>
2021-10-29 14:02:52 +0800

    delete added, tour added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
@@ -16,7 +17,10 @@ const client = new MongoClient(uri);
 async function run() {
     const database = client.db("tourism");
     const toursCollection = database.collection("tours");
+    const tourJoined = database.collection("joined");
 
+
+    // Get All Tour Api 
     try {
         app.get('/tours', async (req, res) => {
             await client.connect();
@@ -29,6 +33,72 @@ async function run() {
         await client.close();
     }
 
+
+    // Get Single Tour Api 
+    try {
+        app.get('/tour/:id', async (req, res) => {
+            await client.connect();
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tour = await toursCollection.findOne(query);
+            res.send(tour);
+        })
+    } finally {
+        await client.close();
+    }
+
+    // Post Joined Tour 
+    try {
+        app.post('/joinedtour', async (req, res) => {
+            await client.connect();
+            const newJoined = req.body;
+            const result = await tourJoined.insertOne(newJoined);
+            res.json(result);
+        })
+    } finally {
+        await client.close();
+    }
+
+    // Get All joined Tours
+    try {
+        app.get('/alljoinedTours', async (req, res) => {
+            await client.connect();
+            const cursor = tourJoined.find({});
+            const joinedTour = await cursor.toArray();
+            res.send(joinedTour);
+        })
+    } finally {
+        await client.close();
+    }
+
+    // get single users joined tours by post 
+
+    try {
+        app.post('/joinedtours/byuser', async (req, res) => {
+            await client.connect();
+            const userEmail = req.body;
+            console.log(userEmail);
+            const query = { email: { $in: userEmail } };
+            const userTours = await tourJoined.find(query).toArray();
+            res.json(userTours);
+        })
+    } finally {
+        await client.close();
+    }
+
+    // delete Single users joined tour 
+    try {
+        app.delete('/deleteJoinedTour/:id', async (req, res) => {
+            await client.connect();
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: ObjectId(id) };
+            const result = await tourJoined.deleteOne(query);
+            res.send(result)
+        })
+    } finally {
+        await client.close();
+    }
 
 }
 run().catch(console.dir);
